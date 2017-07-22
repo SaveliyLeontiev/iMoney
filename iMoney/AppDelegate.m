1f21efde80ed855c87befb9c4211ce03bcb9acc8
Saveliy <sava0796@mail.ru>
2017-07-22 22:48:52 +0700

    Xcode project was created.

---
+++ iMoney/AppDelegate.m
@@ -0,0 +1,28 @@
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    return YES;
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    
+}
+
+
+@end
