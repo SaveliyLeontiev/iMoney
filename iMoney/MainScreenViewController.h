2924828d145e44528920259b19f9a51e2cd13504
Saveliy <sava0796@mail.ru>
2017-07-23 23:57:17 +0700

    Added storyboard screens Added MainScreenViewController Removed useless ViewController

---
+++ iMoney/MainScreenViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface MainScreenViewController : UIViewController
+
+@end
---
+++ iMoney/MainScreenViewController.m
@@ -0,0 +1,18 @@
+#import "MainScreenViewController.h"
+
+
+@interface MainScreenViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *totalSum;
+
+@end
+
+
+@implementation MainScreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
